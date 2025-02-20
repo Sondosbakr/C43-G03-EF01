b2ade3f31b6f6a322ac0b9232edb4eb6afcab1d9
Sondos <Sondosbakr81@gmail.com>
2025-02-20 22:13:57 +0200

    CourseTableWithDataAnnotations

--- Assign01EFCore/Models/Course.cs
+++ Assign01EFCore/Models/Course.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Assign01EFCore.Models
 {
+    [Table("Course")]
     internal class Course
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [Column(name: "Name", TypeName = "varchar")]
+        [MaxLength(50)]
         public string? Name { get; set; }
         public string? Description { get; set; }
         public int TopId { get; set; }
