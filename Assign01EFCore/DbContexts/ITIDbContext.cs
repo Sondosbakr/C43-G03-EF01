39a5a2934b06ded17313a868ec529d21d17fef41
Sondos <Sondosbakr81@gmail.com>
2025-02-20 21:11:05 +0200

    StudentClass

---
+++ Assign01EFCore/DbContexts/ITIDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assign01EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assign01EFCore.DbContexts
+{
+    internal class ITIDbContext : DbContext
+    {
+        public ITIDbContext():base()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = ITIDb; Trusted_Connection = true; Trusted_Connection = true; TrustServerCertificate=True");
+        }
+
+        DbSet<Student> Students { get; set; }
+    }
+}
--- Assign01EFCore/Program.cs
+++ Assign01EFCore/Program.cs
@@ -1,11 +1,13 @@
 ﻿
+using Assign01EFCore.DbContexts;
+
 namespace Assign01EFCore
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            ITIDbContext context = new ITIDbContext();
 
         }
     }
